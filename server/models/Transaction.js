ed2027f46cfa977e68f1a5e1a3201cacb19cbf65
dorababu123-ship-it <dorababudevarapalli.123@gmail.com>
2026-02-26 14:48:56 +0530

    Updated full stack project

---
+++ server/models/Transaction.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["income", "expense"],
+      required: true,
+    },
+    category: String,
+    date: String,
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Transaction", transactionSchema);
